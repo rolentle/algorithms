3d6417588a7b445de2a762ada296643bb7815064
Rolen Le <rolentle@gmail.com>
2017-02-21 10:05:32 -0500

    Rename Node test description

--- js/__tests__/linked-list.test.js
+++ js/__tests__/linked-list.test.js
@@ -1,6 +1,6 @@
 import Node from '../src/linked-list';
 
-test('Node class exists', () => {
+test('Node has value', () => {
   let value = 'foo';
   let node =  new Node(value);
   expect(node.value).toBe(value);
